85d7f302f5c74490a3f02d947e792bc118d8c1e7
lixian <lixian@git.etiantian.com>
2015-07-16 21:29:42 +0800

    forbid instantiate

--- LxVolumeManager/LxVolumeManager.h
+++ LxVolumeManager/LxVolumeManager.h
@@ -6,6 +6,8 @@
 #import <Foundation/Foundation.h>
 #import <CoreGraphics/CoreGraphics.h>
 
+#define LX_UNAVAILABLE(msg) __attribute__((unavailable(msg)))
+
 @interface LxVolumeManager : NSObject
 
 + (CGFloat)currentVolume;
@@ -16,5 +18,10 @@
 
 + (void)beginObserveVolumeChange:(void (^)(CGFloat volume))volumeChangeCallBack;
 + (void)stopObserveVolumeChange;
+
+#pragma mark - Unavailable
+
+- (instancetype)init LX_UNAVAILABLE("LxVolumeManager cannot be initialized!");
+- (instancetype)initWithCoder:(NSCoder *)aDecoder LX_UNAVAILABLE("LxVolumeManager cannot be initialized!");
 
 @end
